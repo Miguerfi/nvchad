daed9e10b69af3f536ce2cba941d5dcfb7c887a2
Miguerfi <miguelpintojose00@gmail.com>
2023-09-03 22:45:27 -0300

    adde bash lang server

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -5,9 +5,10 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
+  b.formatting.shfmt,
   b.formatting.black,
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css","typescriptreact","typescript" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "typescriptreact", "typescript" } }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
